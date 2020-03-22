c170979a2467cf748a219f59b9d99fd310afa64a
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2020-03-22 10:49:02 +0700

    bisa upload puzzle dari txt

--- puzzle.py
+++ puzzle.py
@@ -8,10 +8,10 @@ class Puzzle:
     def __init__(self):
         self.data = []
 
-    #Getter
+    #Getter-----------------------------------------------------------------------------------
     def posisi(self, dataDicari):
         temp = 0
-        for i in range (0,17):
+        for i in range (0,16):
             temp += 1
             if (self.data[i] == dataDicari):
                 break
@@ -33,7 +33,7 @@ class Puzzle:
             for j in range(1,i):
                 if (self.posisi(j) > self.posisi(i)):
                     temp += 1
-        else:
+        else: #Khusus buat i=16
             for j in range(1,i):
                 if (self.posisi(j) > self.posisi(0)):
                     temp += 1
@@ -48,7 +48,16 @@ class Puzzle:
     def isSolvable(self):
         return ((self.sumKurang() + self.cariX())%2 == 0)
 
-    #Setter
+    def calculateCost(self):
+        temp = 0
+        for i in range (0,15):
+            if (self.data[i] != (i+1)):
+                temp += 1
+        if (self.data[15] != 0):
+            temp += 1
+        return temp
+
+    #Setter-------------------------------------------------------------------------------------
     def setData(self, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p):
         self.data = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]
 
@@ -76,34 +85,65 @@ class Puzzle:
         posisiKirinya = self.posisi(0) - 1 - 1
         self.tukarUbin(0, self.data[posisiKirinya])
 
-    #Method
+    #Method----------------------------------------------------------------------------------------------------------------------
     def printPuzzle(self):
         for i in range (0, 16, 4):
-                print(str(self.data[i]) + " " + str(self.data[i + 1]) + " " + str(self.data[i + 2]) + " " + str(self.data[i + 3]))
+                print(str(self.data[i]) + "\t" + str(self.data[i + 1]) + "\t" + str(self.data[i + 2]) + "\t" + str(self.data[i + 3]))
+        print(" ")
 
 class Node:
-        def __init__(self, parent=None, puzzle=[], level=0):
-            self.parent = parent
-            self.puzzle = puzzle
+        def __init__(self, puzzle=[], level=0):
+            self.parent = puzzle
             self.level = level
-    
+            self.nextNode = None
 
+        def nextNode(self, nextNode):
+            self.nextNode = nextNode
+
+def Solve(puzzle):
+    puzzle.printPuzzle()
+    for i in range (1,17):
+        print(str(i) + " = " + str(puzzle.kurang(i)))
+    print(puzzle.isSolvable())
+    print("SumKurang + X = " + str(puzzle.sumKurang() + puzzle.cariX()))
+    if (puzzle.isSolvable() == False):
+        print("Puzzle tidak dapat diselesaikan!!!")
+    else:
+        print("Puzzle bisa diselesaikan")
+
+def bacaPuzzleDariTXT(text_file_name, puzzle):
+    f=open(text_file_name,"r")
+    lines=f.read().splitlines()
+    input=[]
+    for x in lines :
+        temp=x.split(  )
+        temp1= list(map(int, temp))
+        input.append(temp1)
+    f.close()
+    puzzle.data = input[0] + input[1] + input[2] + input[3]
+    
 
 def main1():
     dummy = Puzzle()
-    dummy.setData(1,2,3,4,5,6,0,8,9,10,7,11,13,14,12,15)
+    dummy.setData(1,2,3,4,5,6,0,8,9,10,7,11,13,14,15,12)
     dummy.printPuzzle()
-    dummy.swipeAtas()
+    #dummy.swipeAtas()
     dummy.printPuzzle()
     print(str(dummy.posisi(12)))
     print(str(dummy.dimanaYangKosong()))
     print("X : " + str(dummy.cariX()))
     print("SumKurang : " + str(dummy.sumKurang()))
+    print("Cost : " + str(dummy.calculateCost()))
     print(dummy.isSolvable())
     print("\n\n")
 
     for i in range (1,17):
         print(str(i) + " : " + str(dummy.kurang(i)))
 
-main1()
-print(timeit.timeit(main1, number=0))
+def main2():
+    dummy = Puzzle()
+    bacaPuzzleDariTXT("input.txt", dummy)
+    Solve(dummy)
+
+main2()
+print("Waktu eksekusi :" + str(timeit.timeit(main1, number=0)))
